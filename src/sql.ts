fa219f2bde25b74b3a81b16997a1923bcb13b2b3
umbrik <693254+umbrik@users.noreply.github.com>
2025-05-27 21:27:48 +0200

    Update dependencies

--- src/sql.ts
+++ src/sql.ts
@@ -1,14 +1,5 @@
+import { wshcmxnet } from "@wshcmx/net";
 import { wshcmx } from ".";
-
-type KeyValuePair<K, V> = { Key: K; Value: V };
-
-export interface Sql {
-  Init(connectionString: string): void;
-  ExecuteQuery(commandText: string): { Key: string; Value: unknown }[][];
-  ExecuteNonQuery(commandText: string): void;
-  ExecutePaginationProcedure(procedureName: string, serializedOptions: string, serializedParameters: string): [number, KeyValuePair<string, unknown>[][]];
-  ExecuteProcedure(procedureName: string, serializedParameters: string): [KeyValuePair<string, unknown>[]];
-}
 
 type PaginationProcedureOptions = {
   page?: number;
@@ -42,7 +33,7 @@ export function string(v: unknown) {
 
 export function executeNonQuery<T>(query: string) {
   wshcmx.exception.throwIfNull(wshcmx.net, "wshcmx.net");
-  const sqlInstance = wshcmx.net.CreateClassObject<Sql>("wshcmx.Sql");
+  const sqlInstance = wshcmx.net.CreateClassObject<wshcmxnet.Sql>("wshcmx.Sql");
 
   wshcmx.exception.throwIfNull(sqlInstance, "sqlInstance");
   sqlInstance.Init(wshcmx.connectionString);
@@ -52,12 +43,12 @@ export function executeNonQuery<T>(query: string) {
 
 export function executePaginationProcedure<T>(query: string, options: PaginationProcedureOptions = {}, parameters: Record<string, unknown> = {}) {
   wshcmx.exception.throwIfNull(wshcmx.net, "wshcmx.net");
-  const sqlInstance = wshcmx.net.CreateClassObject<Sql>("wshcmx.Sql");
+  const sqlInstance = wshcmx.net.CreateClassObject<wshcmxnet.Sql>("wshcmx.Sql");
 
   wshcmx.exception.throwIfNull(sqlInstance, "sqlInstance");
   sqlInstance.Init(wshcmx.connectionString);
 
-  const queryResult = sqlInstance.ExecutePaginationProcedure(query, EncodeJson(options), EncodeJson(parameters));
+  const queryResult = sqlInstance.ExecutePaginationProcedure(query, EncodeJson(options), EncodeJson(parameters)) as [number, wshcmxnet.KeyValuePair<string, unknown>[][]];
 
   let pairs;
   let i;
@@ -84,12 +75,12 @@ export function executePaginationProcedure<T>(query: string, options: Pagination
 
 export function executeProcedure<T>(query: string, parameters: Record<string, unknown> = {}) {
   wshcmx.exception.throwIfNull(wshcmx.net, "wshcmx.net");
-  const sqlInstance = wshcmx.net.CreateClassObject<Sql>("wshcmx.Sql");
+  const sqlInstance = wshcmx.net.CreateClassObject<wshcmxnet.Sql>("wshcmx.Sql");
 
   wshcmx.exception.throwIfNull(sqlInstance, "sqlInstance");
   sqlInstance.Init(wshcmx.connectionString);
 
-  const queryResult = sqlInstance.ExecuteProcedure(query, EncodeJson(parameters));
+  const queryResult = sqlInstance.ExecuteProcedure(query, EncodeJson(parameters)) as wshcmxnet.KeyValuePair<string, unknown>[][];
 
   let pairs;
   let i;
@@ -113,7 +104,7 @@ export function executeProcedure<T>(query: string, parameters: Record<string, un
 
 export function executeQuery<T>(query: string) {
   wshcmx.exception.throwIfNull(wshcmx.net, "wshcmx.net");
-  const sqlInstance = wshcmx.net.CreateClassObject<Sql>("wshcmx.Sql");
+  const sqlInstance = wshcmx.net.CreateClassObject<wshcmxnet.Sql>("wshcmx.Sql");
 
   wshcmx.exception.throwIfNull(sqlInstance, "sqlInstance");
   sqlInstance.Init(wshcmx.connectionString);
